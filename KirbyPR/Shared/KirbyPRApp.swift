5f8d4fb2748f760e4efb3f0f5b81d22b85402c36
Fernando Martin Garcia Del Angel <mxg0820@dexcom.com>
2021-06-24 10:46:36 -0700

    [KIRBYPR-0001] Initial Commit
    - Added Universal Project for Kirby PR!

---
+++ KirbyPR/Shared/KirbyPRApp.swift
@@ -0,0 +1,20 @@
+//
+//  KirbyPRApp.swift
+//  Shared
+//
+//  Created by Martin García on 6/24/21.
+//
+
+import SwiftUI
+
+@main
+struct KirbyPRApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
